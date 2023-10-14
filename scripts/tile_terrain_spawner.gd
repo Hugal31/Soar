8f34c1ae5c924373a144eaa89ba78c0026663988
Hugo Laloge <hugo.laloge@gmail.com>
2023-10-14 10:23:29 +0200

    fix(terrain): make terrain loading a lot faster
    Disable duplication, and reuse the same instance instead.

--- scripts/tile_terrain_spawner.gd
+++ scripts/tile_terrain_spawner.gd
@@ -5,7 +5,12 @@ extends Node3D
 @export var width: int = 3
 
 
+const LOGNAME = "TiledTerrainSpawner"
+
+
 func _ready():
+	Logger.add_module(LOGNAME)
+	
 	var tiles = get_children()
 	
 	if tiles.is_empty():
@@ -15,18 +20,17 @@ func _ready():
 	for tile in tiles:
 		remove_child(tile)
 
+	var start_time := Time.get_ticks_msec()
 	var rng := RandomNumberGenerator.new()
 	@warning_ignore("integer_division")
 	var width_side: int = (width - 1) / 2
 	for x in range(-width_side, width_side + 1):
 		for z in range(0, length):
 			var tile = tiles[rng.randi_range(0, tiles.size() - 1)]
-			print("spawn")
-			var new_child: Node3D = tile.duplicate()
-			print("spawned")
+			var new_child: Node3D = tile.duplicate(0)
 			new_child.position = Vector3(x * 2000, 0, z * 2000)
 			if x != 0 or z != 0:
 				new_child.rotation.y = rng.randi_range(0, 3) * PI * 0.5
-			print("add")
 			add_child(new_child)
-			print("added")
+	var end_time := Time.get_ticks_msec()
+	Logger.info("Terrain spawning took %dms" % (end_time - start_time), LOGNAME)

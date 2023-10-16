51a1bd8b9f4354853b9bdec5ef24c1ba854feb97
Hugo Laloge <hugo.laloge@gmail.com>
2023-10-16 22:14:54 +0200

    feat: improve thermal feel

--- entities/winds/thermals/ThermalWindAreaComponent.gd
+++ entities/winds/thermals/ThermalWindAreaComponent.gd
@@ -9,4 +9,13 @@ class_name ThermalWindAreaComponent
 
 
 func get_wind_at_position(position: Vector3):
-	return Vector3(0, 10, 0)
+	var position_2d := parent.global_position
+	position_2d.y = 0
+	var body_position := position
+	body_position.y = 0
+	var distance_to_center := (position_2d - body_position).length()
+	var power := sqrt(radius - distance_to_center)
+	print("Distance: %.1f, power: %.1f" % [distance_to_center, power])
+	if is_nan(power):
+		return Vector3()
+	return Vector3(0, power, 0)

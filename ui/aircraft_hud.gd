143bdf2df62f10c6c7ae1e9ec3359b8a646cd4fe
Hugo Laloge <hugo.laloge@gmail.com>
2023-11-08 19:26:40 +0100

    feat(ui): add variometer and its needle

--- ui/aircraft_hud.gd
+++ ui/aircraft_hud.gd
@@ -1,13 +1,23 @@
 extends Control
 
-
 @export var aircraft: RigidBody3D
 @export var horizontal_speed_label: Label
 @export var vertical_speed_label: Label
+@export var needle: Control
+@export var needle_degrees_per_ms: float = 25.
+@export var needle_max_ms: float = 6.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	var horizontal_velocity := Vector2(aircraft.linear_velocity.x, aircraft.linear_velocity.z)
 	horizontal_speed_label.text = "%.0fkm/h" % (3.6 * horizontal_velocity.length())
-	vertical_speed_label.text = "%.1fm/s" % aircraft.linear_velocity.y
+	var vertical_speed := aircraft.linear_velocity.y
+	vertical_speed_label.text = "%.1fm/s" % vertical_speed
+	if needle:
+		var target_degrees := (
+			signf(vertical_speed)
+			* minf(absf(vertical_speed), needle_max_ms)
+			* needle_degrees_per_ms
+		)
+		needle.rotation_degrees = lerpf(needle.rotation_degrees, target_degrees, 0.5)

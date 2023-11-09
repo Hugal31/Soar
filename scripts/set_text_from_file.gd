1ba4adc0bc3b21f27e1d677545ed48c3055e8ffa
Hugo Laloge <hla@lescompanions.com>
2023-11-09 12:10:17 +0100

    feat(ui): open credit links in browser

--- scripts/set_text_from_file.gd
+++ scripts/set_text_from_file.gd
@@ -7,3 +7,9 @@ extends RichTextLabel
 func _ready():
 	var content := FileAccess.get_file_as_string(file_path)
 	text = content
+
+	meta_clicked.connect(_on_meta_clicked)
+
+
+func _on_meta_clicked(meta):
+	OS.shell_open(meta)
